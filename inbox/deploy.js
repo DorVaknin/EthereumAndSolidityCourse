a193788f21faa9bac41a9ce4422822d1cc1d0133
Dor Vaknin <dor.vkn@gmail.com>
2022-01-30 10:13:26 +0200

    Deploy is now full, need to fix a deployment bug

--- inbox/deploy.js
+++ inbox/deploy.js
@@ -1,6 +1,24 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
 const { interface, bytecode } = require('./compile');
+const fs = require('fs');
+const configData = JSON.parse(fs.readFileSync('account-data.json'));
+const provider = new HDWalletProvider(
+    configData.phrase,
+    'https://rinkeby.infura.io/v3/5c12edf2fa794355b32ed81b7612aeed'
+);
+const web3 = new Web3(provider);
 
-const provider = new HDWalletProvider('
-');
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('Attempting to deploy from account', accounts[0]);
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi there!']})
+    .send({ gas: '100000', from: accounts[0] });
+
+    console.log('Contract deployed to', result.options.address);
+};
+
+deploy();
