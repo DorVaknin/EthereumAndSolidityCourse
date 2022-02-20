cc20df1d1bae6bc5598b958b785f2a91fcfb2a8e
Dor Vaknin <dor.vkn@gmail.com>
2022-02-20 19:16:01 +0200

    fixed bug

--- inbox/deploy.js
+++ inbox/deploy.js
@@ -1,6 +1,7 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
 const { interface, bytecode } = require('./compile');
+
 const fs = require('fs');
 const configData = JSON.parse(fs.readFileSync('account-data.json'));
 const provider = new HDWalletProvider(
@@ -10,15 +11,15 @@ const provider = new HDWalletProvider(
 const web3 = new Web3(provider);
 
 const deploy = async () => {
-    const accounts = await web3.eth.getAccounts();
+  const accounts = await web3.eth.getAccounts();
 
-    console.log('Attempting to deploy from account', accounts[0]);
+  console.log('Attempting to deploy from account', accounts[0]);
 
-    const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ['Hi there!']})
-    .send({ gas: '100000', from: accounts[0] });
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0] });
 
-    console.log('Contract deployed to', result.options.address);
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
 };
-
 deploy();
